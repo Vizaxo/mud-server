1bb3dc539bed428aec509fefe437d3a2d290edef
Vizaxo <vizaxo@mailfence.com>
2018-11-11 02:53:10 +0000

    Add thread-local and global state

--- lib/Mud.hs
+++ lib/Mud.hs
@@ -4,16 +4,25 @@ import Networking
 import World
 import Player
 
+import Control.Monad.Reader
+import Control.Monad.Writer
 import Data.Monoid
 
-greeting :: Communicate ()
+data ClientState = LoggedIn Player | NotLoggedIn | EnteredName
+
+greeting :: Communicate ClientState World ()
 greeting = do
-  write "Welcome to the MUD!\n"
-  write "What is your name?\n"
-  name <- receive
-  write $ "Welcome, " <> name <> "!\n"
-  let player = Player Spawn name
-  write (show player)
+  getLocal >>= \case
+    NotLoggedIn -> do
+      ask --TODO: don't make user enter first message
+      tell "Welcome to the MUD!\nWhat is your name?"
+      setLocal EnteredName
+    EnteredName -> do
+      name <- ask
+      setLocal (LoggedIn (Player Spawn name))
+      tell $ "Hello, " <> name
+    LoggedIn (Player location name) -> do
+      tell $ name <> ", you are in " <> show location
 
 runMud :: Int -> IO ()
-runMud = run greeting
+runMud = run NotLoggedIn World greeting
--- lib/Networking.hs
+++ lib/Networking.hs
@@ -1,26 +1,31 @@
 module Networking where
 
 import Control.Concurrent
+import Control.Concurrent.STM
 import Control.Monad.Reader
+import Control.Monad.Writer hiding (listen)
+import Control.Monad.State
 import Data.Monoid
 import System.Environment
-import Text.Read
+import Text.Read hiding (get)
 
 import qualified Data.ByteString.Char8 as CBS
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 
-newtype Communicate a = Communicate { unComm :: ReaderT Socket IO a }
-  deriving (Functor, Applicative, Monad, MonadIO, MonadReader Socket)
-
-runComm :: Communicate a -> Socket -> IO a
-runComm = runReaderT . unComm
+-- | Communication monad.
+-- Reader String: user input
+-- Writer String: output to user
+-- State l: connection-local state
+-- State g: global state
+newtype Communicate l g a = Communicate { unComm :: ReaderT String (WriterT String (State (l, g))) a }
+  deriving (Functor, Applicative, Monad, MonadReader String, MonadWriter String, MonadState (l, g))
 
 main :: IO ()
 main = do getArgs >>= \case
-            []     -> run echo 8080
+            []     -> run 0 0 echo 8080
             [port] -> case readMaybe port of
-                        Just p -> run echo p
+                        Just p -> run 0 0 echo p
                         Nothing -> usage
             _      -> usage
 
@@ -28,8 +33,8 @@ usage :: IO ()
 usage = do name <- getProgName
            putStrLn $ "Usage: " <> name <> " [port]"
 
-run :: Communicate a -> Int -> IO a
-run f port = withSocketsDo $ do
+run :: forall l g a. l -> g -> Communicate l g a -> Int -> IO a
+run l g f port = withSocketsDo $ do
   addr <- resolve $ show port
   sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
   setSocketOption sock ReuseAddr 1
@@ -38,9 +43,23 @@ run f port = withSocketsDo $ do
   setCloseOnExecIfNeeded fd
   listen sock 5
 
+  global <- newTVarIO g
+
   forever $ do
     (conn,peer) <- accept sock
-    forkFinally (runComm f conn) (const $ close conn)
+    forkFinally (run conn l global) (const $ close conn)
+  where
+    run :: Socket -> l -> TVar g -> IO ()
+    run conn l global = do
+      msg <- receive conn
+      when (msg == ":quit") mzero
+      (l', output) <- atomically $ do
+        g <- readTVar global
+        let ((x, output), (l', g')) = runState (runWriterT (runReaderT (unComm f) msg)) (l, g)
+        writeTVar global g'
+        return (l', output)
+      write (output <> "\n") conn
+      run conn l' global
 
 resolve port = do
   let hints = defaultHints
@@ -50,15 +69,38 @@ resolve port = do
   addr:_ <- getAddrInfo (Just hints) Nothing (Just port)
   return addr
 
-write :: String -> Communicate ()
-write str = do
-  sock <- ask
-  void $ liftIO $ send sock (CBS.pack str)
+write str sock = void $ send sock (CBS.pack str)
 
-receive :: Communicate String
-receive = do
-  sock <- ask
-  liftIO $ (init . init . CBS.unpack) <$> recv sock 1024
+receive sock = do
+  msg <- recv sock 1024
+  when (CBS.length msg == 0) mzero
+  return (init . init . CBS.unpack $ msg)
 
-echo :: Communicate ()
-echo = write "connection established" >> forever (receive >>= write)
+-- | Echoes back what the user wrote. Keeps a count of the numeber of
+-- messages sent per connection, and the total number of messages sent
+-- to the server.
+echo :: Communicate Int Int ()
+echo = do
+  ask >>= tell
+  (l, g) <- get
+  tell ("local count:" <> show l <> "\n")
+  tell ("global count:" <> show g)
+  modify (\(l, g) -> ((l+1), (g+1)))
+
+setLocal :: l -> Communicate l g ()
+setLocal l = modify (\(_, g) -> (l, g))
+
+setGlobal :: g -> Communicate l g ()
+setGlobal g = modify (\(l, _) -> (l, g))
+
+getLocal :: Communicate l g l
+getLocal = fst <$> get
+
+getGlobal :: Communicate l g g
+getGlobal = snd <$> get
+
+modifyLocal :: (l -> l) -> Communicate l g ()
+modifyLocal f = modify (\(l, g) -> (f l, g))
+
+modifyGlobal :: (g -> g) -> Communicate l g ()
+modifyGlobal f = modify (\(l, g) -> (l, f g))
