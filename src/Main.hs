b281a7f721512b6ba8a481044bba039af3692536
Vizaxo <vizaxo@mailfence.com>
2018-11-12 01:01:21 +0000

    Add server executable

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,19 @@
 module Main where
 
+import Mud
+
+import Data.Monoid
+import System.Environment
+import Text.Read
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = do getArgs >>= \case
+            []     -> server 8080
+            [port] -> case readMaybe port of
+                        Just p -> server p
+                        Nothing -> usage
+            _      -> usage
+
+usage :: IO ()
+usage = do name <- getProgName
+           putStrLn $ "Usage: " <> name <> " [port]"
