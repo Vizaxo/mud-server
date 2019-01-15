0a83609e4ddb3f76611a8a890811c8e0ddd8e5ae
Vizaxo <vizaxo@mailfence.com>
2019-01-15 19:18:44 +0000

    Remove saving features

--- lib/ConsoleControl.hs
+++ lib/ConsoleControl.hs
@@ -10,40 +10,30 @@ import Text.Parsec.Char
 import System.Exit
 
 import GameState
-import LoadSave
 import Utils
 
 data ConsoleEvent = ConsoleMessage String
   deriving Show
 
-data ConsoleCommand = Save | SaveAndQuit | Quit
+data ConsoleCommand = Quit
   deriving Show
 
 data ConsoleProcess
   = IOAction (forall m. (MonadIO m, MonadReader GameState m) => m ())
   | ConsoleOutputMessage String
 
-instance Show ConsoleProcess where
-  show (IOAction _) = "IOAction <function>"
-  show (ConsoleOutputMessage msg) = "ConsoleOutputMessage " <> show msg
-
 consoleCommand :: Parser ConsoleCommand
-consoleCommand = asum (mkSimple <$> [Save, SaveAndQuit, Quit]) <* eof
+consoleCommand = asum (mkSimple <$> [Quit]) <* eof
 
 processConsoleEvent :: ConsoleEvent -> [ConsoleProcess]
 processConsoleEvent (ConsoleMessage msg) = case parse consoleCommand "" msg of
   Left e -> [ConsoleOutputMessage $ "Error parsing command " <> msg]
   Right cmd -> case cmd of
-    Save -> [IOAction save]
-    SaveAndQuit -> [IOAction (save >> quit)]
     Quit -> [IOAction quit]
 
 handleConsoleProcess :: MonadIO m => GameState -> ConsoleProcess -> m ()
 handleConsoleProcess gs (IOAction fm) = runReaderT fm gs
 handleConsoleProcess gs (ConsoleOutputMessage msg) = liftIO $ putStrLn (msg <> "\n")
-
-save :: (MonadIO m, MonadReader GameState m) => m ()
-save = (liftIO $ putStrLn "saving") >> (saveGame =<< ask)
 
 quit :: MonadIO m => m ()
 quit = liftIO exitSuccess
